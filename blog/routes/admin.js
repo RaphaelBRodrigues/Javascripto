9f7e411670e313b3c508b2c663c17619c0ffa652
raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-07-12 13:31:09 -0300

    Cadastro/validação e listagem de categorias

--- blog/routes/admin.js
+++ blog/routes/admin.js
@@ -17,30 +17,68 @@ router.get("/posts",(req,res)=>{
 })
 
 router.get("/categorias",(req,res)=>{
-    res.render("admin/categorias");
+    Categoria.find().lean().then((categorias) =>{
+        res.render("admin/categorias",{categorias:categorias});
+        console.log(categorias);
+    }).catch((e)=>{
+        req.flash("error_msg","Erro na listagem das categorias");
+        res.redirect("/admin");
+    });
+    console.log("Categorias");
+
 })
 
 router.get("/categorias/add",(req,res)=>{
     res.render("admin/addCategoria");
 });
 
+
+
 router.post("/categorias/novas",(req,res)=>{
     const novaCategoria = {
         nome: req.body.nome,
         slug: req.body.slug
-
         
     }
     
-    
-    new Categoria(novaCategoria).save().then(()=>{
-        console.log("Categoria cadastrada");
-    }).catch(()=>{
-        console.log("Erro no cadastro da categoria");
-    });
-    res.send("das");
-});
 
+
+
+    var erros = [];
+    
+
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({text:"Nome inválido"});
+    }
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({text:"Slug inválido"});
+    }
+    if(req.body.nome.lenght < 3){
+        erros.push({text:"Nome da categoria muito pequeno"});
+    }
+    
+
+    if(erros.length > 0){
+        res.render("admin/status",{erros:erros});
+       }else{  
+            new Categoria(novaCategoria).save().then(()=>{
+                console.log("Categoria cadastrada");
+                req.flash("success_msg","Categoria cadastrada com sucesso");
+                res.redirect("/admin/categorias");
+
+
+            }).catch(()=>{
+                console.log("Erro no cadastro da categoria");
+                req.flash("error_msg","Falha no cadastro da categoria");
+                res.redirect("/admin/");
+
+
+            });
+            //res.render("admin/status");
+        }
+
+});
 
 
 module.exports = router;
