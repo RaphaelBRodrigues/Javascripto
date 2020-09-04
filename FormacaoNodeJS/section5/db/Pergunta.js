55ac62f4a1f6b3651937607dd8c1ce915161ac40
raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-09-04 17:45:34 -0300

    Criacao do model Perguntas

---
+++ FormacaoNodeJS/section5/db/Pergunta.js
@@ -0,0 +1,12 @@
+const Sequelize = require("sequelize");
+const connection = require("./database");
+
+
+const Pergunta = connection.define("Pergunta",{
+	titulo:{type:Sequelize.STRING,allowNull:false},
+	descricao:{type:Sequelize.TEXT,allowNull:false}
+},{});
+
+Pergunta.sync({force:false}).then(()=>{
+	console.log("Tabela Pergunta criada");
+});
