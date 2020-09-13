4ef2957ddd5a67bb560a9dca78c6a4203a4fa4a3
raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-09-13 19:42:22 -0300

    Criação do projeto Vue + componentes teste

---
+++ FormacaoNodeJS/section21/guiapress/src/components/Cliente.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="cliente">
+    <h2>
+      {{ nome }}
+    </h2>
+    <h3>
+      {{ descricao }}
+    </h3>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      nome:"Raphael",
+      numero:"",
+      descricao:"dasadsaads sadsad asdsa"
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #cliente{
+    color:blue;
+    background-color: black;
+  }
+</style>
---
+++ FormacaoNodeJS/section21/guiapress/src/components/Produto.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>
+      Produto
+    </h2>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
