a321a1e34ce47a474d3dc236ea631baa71a4b814
raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-09-13 20:11:27 -0300

    Props,data(),v-model / data-bind : / v-show / v-if(else if else)

--- FormacaoNodeJS/section21/guiapress/src/components/Cliente.vue
+++ FormacaoNodeJS/section21/guiapress/src/components/Cliente.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="cliente">
-    <h2>
-      {{ nome }}
-    </h2>
-    <h3>
-      {{ descricao }}
-    </h3>
+    <h1>CLIENTE </h1>
+    <h4 :v-if="cliente.nome">
+    Nome:   {{ cliente.nome }}
+    </h4>
+    <p>
+    Descrição:  {{ cliente.descricao }}
+    </p>
+    <p>
+     Número: {{ cliente.numero }}
+    </p>
+    <p v-show="showIdade">
+      Idade: {{ cliente.idade }}
+    </p>
+    <p v-if="showIdade">
+      Idade: {{ cliente.idade }}
+    </p>
+    <p v-else-if="false">Idade oculta else</p>
+    <p v-else>Idade oculta</p>
+    <p>
+      Email: {{ cliente.email }}
+    </p>
   </div>
 
 </template>
 
 <script>
 export default {
-  data(){
-    return {
-      nome:"Raphael",
-      numero:"",
-      descricao:"dasadsaads sadsad asdsa"
-    }
+  props:{
+    cliente: Object,
+    showIdade:Boolean
   }
 }
 </script>
@@ -25,6 +37,5 @@ export default {
 <style scoped>
   #cliente{
     color:blue;
-    background-color: black;
   }
 </style>
