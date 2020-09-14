3d6b0b46b3a18adbc1d3bcb83df3793ac4527f08
raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-09-14 18:55:08 -0300

    Criação do projeto pokevue com Vue.js

---
+++ FormacaoNodeJS/section22/pokevue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+const pokeapi = axios.create({
+  baseURL:"https://pokeapi.co/api/v2/"
+});
+
+async function main(){
+  const pokemons = await pokeapi.get("pokemon?limit=150&offset=50");
+  console.table(pokemons);
+}
+
+main();
+
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
