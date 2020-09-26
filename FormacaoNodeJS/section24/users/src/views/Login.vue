cc8a2254d606f6762b193c3d1522139f0ee9af3d
raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-09-26 20:51:48 -0300

    Registro,login e gerenciamento de acessos dos usuários

---
+++ FormacaoNodeJS/section24/users/src/views/Login.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+      <h2>
+        Login
+      </h2>
+      <hr>
+      <div class="columns is-mobile is-centered">
+        <div class="column is-half">
+          <div class="notification is-danger" v-if="error != ''">
+                <p>{{ error }}</p>
+          </div>
+          <label>Email:</label>
+          <input v-model="email" type="email" name="email" class="input">
+
+          <label>Senha:</label>
+          <input v-model="password" type="password" name="password" class="input">
+          <button @click="login" class="button is-success">Logar</button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+
+
+export default {
+  data(){
+    return{
+      password:"",
+      email:"",
+      error:""
+    };
+  },
+  methods:{
+    async login(){
+      try{
+        const resp = await axios.post("http://localhost:8686/auth",{
+          password:this.password,
+          email:this.email,
+        });
+
+
+        const token = resp.data.token;
+        console.log(token);
+
+
+        if(token) {
+          localStorage.setItem("token",token);
+          this.$router.push({name: "Home"});
+        }else{
+          console.log("erro");
+          throw new Error("Credenciais inválidas");
+        }
+      }catch (err){
+        console.log(err);
+
+        this.error = err.message;
+      }
+
+    }
+
+  }
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ FormacaoNodeJS/section24/users/src/views/Users.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+      <h2>
+        Admin
+      </h2>
+      <hr>
+      <div class="columns is-mobile is-centered is-disabled">
+        <div class="column is-half">
+          <div class="notification is-danger" v-if="error != ''">
+                <p>{{ error }}</p>
+          </div>
+          <label>Email:</label>
+          <input v-model="email" type="email" name="email" class="input">
+
+          <label>Nome:</label>
+          <input v-model="name" type="text" name="name" class="input">
+
+          <label>Senha:</label>
+          <input v-model="password" type="password" name="password" class="input">
+          <button @click="register" class="button is-success">Cadastrar</button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+
+
+export default {
+  data(){
+    return{
+      name:"",
+      password:"",
+      email:"",
+      error:""
+    };
+  },
+  methods:{
+    async register(){
+      try{
+        await axios.post("http://localhost:8686/user",{
+          name:this.name,
+          password:this.password,
+          email:this.email,
+        });
+        this.$router.push({name:"Home"});
+      }catch (err){
+        this.error = err.response.data.err;
+      }
+
+    }
+
+  }
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
